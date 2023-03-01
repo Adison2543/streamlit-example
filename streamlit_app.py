8dfbe303ad8bb95563f31eef5adbe438dedb506e
Adison Wonglakhon <58928791+Adison2543@users.noreply.github.com>
2023-03-01 15:24:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,25 +12,6 @@ With CarieDetect, simply upload your x-ray image. Our machine learning algorithm
 The system will then show you the results and details below.
 """
 
-img = st.file_uploader("Upload an X-ray Image")
+img = st.file_uploader("First!. Please Upload an X-ray Image to detect a caries spot")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
