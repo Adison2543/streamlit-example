1ac21c2783c9d78d026c8cf58e1ca9ace5d8ec79
Adison Wonglakhon <58928791+Adison2543@users.noreply.github.com>
2023-03-01 16:33:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,31 +17,27 @@ img = st.file_uploader("First!. Please Upload an X-ray Image to detect a caries 
 st.write("If you have successfully uploaded the image. Please press the 'Process' button to evaluate.")
 clicked = st.button("Process")
 
-"""
 
-
-# Result
-
+with st.expander("See result"):
+    """
+    # Result
+    """
     
-"""
+    
+    chart_data = pd.DataFrame(
+        np.random.randn(100,2),
+        columns=["Accuracy", "Loss"]
+    )
+    st.write("Display a line chart of Accuracy and Loss of result")
+    st.line_chart(chart_data)
 
 
-
-st.write("\n \n")
-chart_data = pd.DataFrame(
-    np.random.randn(100,2),
-    columns=["Accuracy", "Loss"]
-)
-st.write("Display a line chart of Accuracy and Loss of result")
-st.line_chart(chart_data)
+    st.image("https://cdn.discordapp.com/attachments/886148973386162196/1080421305893011476/resEx.png")
 
 
-st.image("https://cdn.discordapp.com/attachments/886148973386162196/1080421305893011476/resEx.png")
-
-
-st.write("Display a Accuracy and Loss of each class")
-col1, col2, col3, col4 = st.columns(4)
-col1.metric("Caries", "55%", "- loss = 50%")
-col2.metric("Enamel", "78%", "- loss = 20%")
-col3.metric("Pulp", "80%", "- loss = 20%")
-col4.metric("Tooth", "70%", "- loss = 20%")
+    st.write("Display a Accuracy and Loss of each class")
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("Caries", "55%", "- loss = 50%")
+    col2.metric("Enamel", "78%", "- loss = 20%")
+    col3.metric("Pulp", "80%", "- loss = 20%")
+    col4.metric("Tooth", "70%", "- loss = 20%")
