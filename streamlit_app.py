563a79a9bc1e761abfe1c4d0cf163dad75ef911a
Adison Wonglakhon <58928791+Adison2543@users.noreply.github.com>
2023-03-01 14:50:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Adison Web App!
+# Welcome to Caries Detection!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
