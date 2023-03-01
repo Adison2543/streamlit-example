f8f60008f0257118b6d4281f70876fb108616d59
Adison Wonglakhon <58928791+Adison2543@users.noreply.github.com>
2023-03-01 17:05:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,9 @@ The system will then show you the results and details below.
 """
 
 img = st.file_uploader("First!. Please Upload an X-ray Image to detect a caries spot")
-st.write("Example image")
 if (img) :
-    st.image(img, caption='Your uploaded image')
+    st.write("Example image")
+    st.image(img, caption='Your uploaded image', width=20)
 
 st.write("If you have successfully uploaded the image. Please press the 'Process' button to evaluate.")
 clicked = st.button("Process")
@@ -31,27 +31,26 @@ if (clicked) :
         my_bar.progress(i + 1, text=progress_text)
            
     st.write("✔️ Done!")
-
-with st.expander("See result"):
-    """
-    # Result
-    """
-    
-    
-    chart_data = pd.DataFrame(
-        np.random.randn(100,2),
-        columns=["Accuracy", "Loss"]
-    )
-    st.write("Display a line chart of Accuracy and Loss of result")
-    st.line_chart(chart_data)
-
-
-    st.image("https://cdn.discordapp.com/attachments/886148973386162196/1080421305893011476/resEx.png")
+    with st.expander("See result"):
+        """
+        # Result
+        """
+        
+        
+        chart_data = pd.DataFrame(
+            np.random.randn(100,2),
+            columns=["Accuracy", "Loss"]
+        )
+        st.write("Display a line chart of Accuracy and Loss of result")
+        st.line_chart(chart_data)
 
 
-    st.write("Display a Accuracy and Loss of each class")
-    col1, col2, col3, col4 = st.columns(4)
-    col1.metric("Caries", "55%", "- loss = 50%")
-    col2.metric("Enamel", "78%", "- loss = 20%")
-    col3.metric("Pulp", "80%", "- loss = 20%")
-    col4.metric("Tooth", "70%", "- loss = 20%")
+        st.image("https://cdn.discordapp.com/attachments/886148973386162196/1080421305893011476/resEx.png")
+
+
+        st.write("Display a Accuracy and Loss of each class")
+        col1, col2, col3, col4 = st.columns(4)
+        col1.metric("Caries", "55%", "- loss = 50%")
+        col2.metric("Enamel", "78%", "- loss = 20%")
+        col3.metric("Pulp", "80%", "- loss = 20%")
+        col4.metric("Tooth", "70%", "- loss = 20%")
