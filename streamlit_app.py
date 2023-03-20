5c68dbb63955e9fe920adf144afce04d26db3ecd
Adison Wonglakhon <58928791+Adison2543@users.noreply.github.com>
2023-03-20 14:25:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,20 +34,27 @@ if (clicked) :
         st.success("✔️ Done!")
         
         st.image("https://cdn.discordapp.com/attachments/886148973386162196/1080421305893011476/resEx.png")
+        
+        with st.expander("See result"):
+            """
+            # Result
+            """
 
-        st.write("Display a Accuracy and Loss of each class")
-        col1, col2, col3, col4 = st.columns(4)
-        col1.metric("Caries", "55%", "- loss = 50%")
-        col2.metric("Enamel", "78%", "- loss = 20%")
-        col3.metric("Pulp", "80%", "- loss = 20%")
-        col4.metric("Tooth", "70%", "- loss = 20%")
             
-        chart_data = pd.DataFrame(
-            np.random.randn(100,2),
-            columns=["Accuracy", "Loss"]
-        )
-        st.write("Display a line chart of Accuracy and Loss of result")
-        st.line_chart(chart_data)
+
+            st.write("Display a Accuracy and Loss of each class")
+            col1, col2, col3, col4 = st.columns(4)
+            col1.metric("Caries", "55%", "- loss = 50%")
+            col2.metric("Enamel", "78%", "- loss = 20%")
+            col3.metric("Pulp", "80%", "- loss = 20%")
+            col4.metric("Tooth", "70%", "- loss = 20%")
+            
+            chart_data = pd.DataFrame(
+                np.random.randn(100,2),
+                columns=["Accuracy", "Loss"]
+            )
+            st.write("Display a line chart of Accuracy and Loss of result")
+            st.line_chart(chart_data)
             
         clicked_reset = st.button("Reset")
         if (clicked_reset):
